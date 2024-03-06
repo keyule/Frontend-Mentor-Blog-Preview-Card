8c7c856e96e0f50c5803ddf9be7869ed97d665fe
keyule <keyule@gmail.com>
2024-03-06 12:08:10 +0800

    set up gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Frontend-Mentor-Blog-Preview-Card/",
   plugins: [react()],
 })
